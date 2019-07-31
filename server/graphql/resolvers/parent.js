6797be25a6b4e3236c5e968bccf77490443ad751
Mary Etokwudo <etokwudomary@gmail.com>
2019-07-31 11:52:47 +0100

    create resolvers for query and mutations for parent-users

---
+++ server/graphql/resolvers/parent.js
@@ -0,0 +1,89 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+
+const { SECRET_KEY } = require('../../config');
+const Parent = require('../../models/Parent');
+
+const resolvers = {
+	Query: {
+		async getParents() {
+			try {
+				const parents = await Parent.find();
+				return parents;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	},
+	Mutation: {
+		async parentSignUp(
+			_,
+			{
+				parentSignUpInput: {
+					firstName,
+					lastName,
+					email,
+					phone,
+					gender,
+					stateOfRes,
+					location,
+					password,
+					confirmPassword,
+					userRole,
+				},
+			}
+		) {
+			//validate parent data
+
+			// make sure parent doesn't already exist
+			const parent = await Parent.findOne({ email });
+			if (parent) {
+				throw new UserInputError('email already exists in database', {
+					errors: {
+						email: 'This email already exists in database',
+					},
+				});
+			}
+
+			// hash password and create an auth token
+			password = await bcrypt.hash(password, 12);
+			const newParent = new Parent({
+				firstName,
+				lastName,
+				email,
+				phone,
+				gender,
+				stateOfRes,
+				location,
+				password,
+				userRole,
+				createdAt: new Date().toISOString(),
+			});
+			const res = await newParent.save();
+			const token = jwt.sign(
+				{
+					id: res.id,
+					firstName: res.firstName,
+					lastName: res.lastName,
+					email: res.email,
+					phone: res.phone,
+					gender: res.gender,
+					stateOfRes: res.stateOfRes,
+					location: res.location,
+					userRole: res.userRole,
+				},
+				SECRET_KEY,
+				{ expiresIn: '1h' }
+			);
+
+			return {
+				...res._doc,
+				id: res._id,
+				token,
+			};
+		},
+	},
+};
+
+module.exports = resolvers;
