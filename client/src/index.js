96c932767927fd6fbc6383827e20c40985e9fb78
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-02 14:11:56 +0100

    install and implement Apollo

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const httpLink = createHttpLink({
+	uri: 'http://localhost:5000',
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
+	<ApolloProvider client={client}>
+		<Router>
+			<App />
+		</Router>
+	</ApolloProvider>,
 	document.getElementById('root')
 );
 
