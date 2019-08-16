c960805b152cdae5fe461e8eb75b65ded90006dc
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-16 18:23:23 +0100

    update navbar to highlight current page

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
 export default function NavBar() {
+	const pathname = window.location.pathname;
+
+	const path = pathname === '/' ? 'upthrust-tutors' : pathname.substr(1);
+	const [activeItem, setActiveItem] = useState(path);
+	const handleItemClick = (e, { name }) => setActiveItem(name);
+
 	return (
-		<div style={{ borderWidth: '0px' }} className='ui stackable inverted pointing secondary menu'>
+		<Menu style={{ borderWidth: '0px' }} className='ui' stackable inverted pointing secondary>
 			<div className='ui container'>
-				<Link to='/' className='item'>
-					Upthrust Tutors
-				</Link>
-				<Link to='/become-a-tutor' className='item'>
-					Become a Tutor
-				</Link>
-				<Link to='/get-a-tutor' className='item'>
-					Get a Tutor
-				</Link>
-				<Link to='/tutor-requests' className='item'>
-					View Requests
-				</Link>
-				<Link to='/contact-us' className='item'>
-					Contact
-				</Link>
+				<Menu.Item
+					as={Link}
+					name='upthrust-tutors'
+					active={activeItem === 'upthrust-tutors'}
+					onClick={handleItemClick}
+					to='/'
+				/>
+				<Menu.Item
+					as={Link}
+					name='become-a-tutor'
+					active={activeItem === 'become-a-tutor'}
+					onClick={handleItemClick}
+					to='/become-a-tutor'
+				/>
+				<Menu.Item
+					as={Link}
+					name='get-a-tutor'
+					active={activeItem === 'get-a-tutor'}
+					onClick={handleItemClick}
+					to='/get-a-tutor'
+				/>
+				<Menu.Item
+					as={Link}
+					name='tutor-requests'
+					active={activeItem === 'tutor-requests'}
+					onClick={handleItemClick}
+					to='/tutor-requests'
+				/>
+				<Menu.Item
+					as={Link}
+					name='contact-us'
+					active={activeItem === 'contact-us'}
+					onClick={handleItemClick}
+					to='/contact-us'
+				/>
 				<div className='right item'>
 					<Link to='/login' style={{ backgroundColor: '#b32424' }} className='ui inverted button' role='button'>
-						Log in
+						Login
 					</Link>
 				</div>
 			</div>
-		</div>
+		</Menu>
 	);
 }
